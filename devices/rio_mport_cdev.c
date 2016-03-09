1a3c573e70fb2ac04e2fc6dcc905a256812d8efc
Joe Perches <joe@perches.com>
2016-03-09 11:49:48 -0500

    treewide: Remove unnecessary 0x prefixes before %pa extension uses
    Since commit 3cab1e711297 ("lib/vsprintf: refactor duplicate code
    to special_hex_number()") %pa uses have been ouput with a 0x prefix.
    
    These 0x prefixes in the formats are unnecessary.
    
    Signed-off-by: Joe Perches <joe@perches.com>
    Signed-off-by: Alexandre Bounine <alexandre.bounine@idt.com>

--- devices/rio_mport_cdev.c
+++ devices/rio_mport_cdev.c
@@ -2238,7 +2238,7 @@ static void mport_mm_open(struct vm_area_struct *vma)
 {
 	struct rio_mport_mapping *map = vma->vm_private_data;
 
-rmcd_debug(MMAP, "0x%pad", &map->phys_addr);
+	rmcd_debug(MMAP, "%pad", &map->phys_addr);
 	kref_get(&map->ref);
 }
 
@@ -2246,7 +2246,7 @@ static void mport_mm_close(struct vm_area_struct *vma)
 {
 	struct rio_mport_mapping *map = vma->vm_private_data;
 
-rmcd_debug(MMAP, "0x%pad", &map->phys_addr);
+	rmcd_debug(MMAP, "%pad", &map->phys_addr);
 	mutex_lock(&map->md->buf_mutex);
 	kref_put(&map->ref, mport_release_mapping);
 	mutex_unlock(&map->md->buf_mutex);
