65daafc64525b7391fc6d8af910b30df098247c5
Chris Warrington <christopher.warrington.fv@renesas.com>
2019-12-02 16:21:02 -0600

    Only support DTYPE1

--- goodput/common/tsi721_umd/src/tsi721_umd_dma.c
+++ goodput/common/tsi721_umd/src/tsi721_umd_dma.c
@@ -12,9 +12,9 @@ static inline uint32_t le32(const uint32_t n)
 }
 
 void tsi721_umd_create_dma_descriptor (tsi721_dma_desc* bd_ptr, 
-                uint16_t devid, uint32_t bcount,
-                uint64_t raddr_lsb64, uint8_t raddr_msb2,
-                uint8_t *buffer_ptr)
+	            uint16_t devid, uint32_t bcount,
+	            uint64_t raddr_lsb64, uint8_t raddr_msb2,
+	            uint8_t *buffer_ptr)
 
 {
 	const uint8_t rtype = ALL_NWRITE;
@@ -24,31 +24,25 @@ void tsi721_umd_create_dma_descriptor (tsi721_dma_desc* bd_ptr,
 
 	enum dma_dtype dtype = DTYPE1;
 
-        memset(bd_ptr, 0, sizeof(*bd_ptr));
+	memset(bd_ptr, 0, sizeof(*bd_ptr));
 
-	if (bcount <= 16) {
-		dtype = DTYPE2;
-                if (rtype != NREAD)
-                        memcpy(&bd_ptr->data[0], buffer_ptr, 16);
-	} else {
-		if (bcount == (TSI721_DMAD_BCOUNT1+1))
-			bcount = 0;
-		bcount &= TSI721_DMAD_BCOUNT1;
-                bd_ptr->t1.bufptr_lo = le32((uint64_t)buffer_ptr & 0xffffffff);
-                bd_ptr->t1.bufptr_hi = le32((uint64_t)buffer_ptr >> 32);
-	};
+	if (bcount == (TSI721_DMAD_BCOUNT1+1))
+		bcount = 0;
+	bcount &= TSI721_DMAD_BCOUNT1;
+	        bd_ptr->t1.bufptr_lo = le32((uint64_t)buffer_ptr & 0xffffffff);
+	        bd_ptr->t1.bufptr_hi = le32((uint64_t)buffer_ptr >> 32);
 
-        bd_ptr->type_id = le32((dtype << 29) |
-                        (rtype << 19) | (prio << 17) | (crf << 16) |
-                        (uint32_t)devid);
+	bd_ptr->type_id = le32((dtype << 29) |
+	                (rtype << 19) | (prio << 17) | (crf << 16) |
+	                (uint32_t)devid);
 
-        bd_ptr->bcount = le32(((raddr_lsb64 & 0x3) << 30) |
-                        (tt << 26) | bcount);
+	bd_ptr->bcount = le32(((raddr_lsb64 & 0x3) << 30) |
+	                (tt << 26) | bcount);
 
-        raddr_lsb64 = ((uint64_t)(raddr_msb2 & 0x3) << 62) |
-                        (raddr_lsb64 >> 2);
+	raddr_lsb64 = ((uint64_t)(raddr_msb2 & 0x3) << 62) |
+	                (raddr_lsb64 >> 2);
 
-        bd_ptr->raddr_lo = le32(raddr_lsb64 & 0xffffffff);
-        bd_ptr->raddr_hi = le32(raddr_lsb64 >> 32);
+	bd_ptr->raddr_lo = le32(raddr_lsb64 & 0xffffffff);
+	bd_ptr->raddr_hi = le32(raddr_lsb64 >> 32);
 };
 
